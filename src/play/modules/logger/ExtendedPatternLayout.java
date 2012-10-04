3e1e72aa9ddb63bd1a57e7b6b4ce30b5e8c8bdab
Anton Keks <anton@codeborne.com>
2012-10-04 12:44:20 +0300

    do not share pattern converters among different appenders

--- src/play/modules/logger/ExtendedPatternLayout.java
+++ src/play/modules/logger/ExtendedPatternLayout.java
@@ -6,10 +6,6 @@ import org.apache.log4j.spi.LoggingEvent;
 import play.mvc.Http;
 
 public class ExtendedPatternLayout extends org.apache.log4j.PatternLayout {
-
-  private final static PatternConverter HEAP_SIZE_PATTERN_CONVERTER = new HeapSizePatternConverter();
-  private final static PatternConverter REQUEST_ID_PATTERN_CONVERTER = new RequestIdPatternConverter();
-
   public ExtendedPatternLayout() {
   }
 
@@ -22,10 +18,10 @@ public class ExtendedPatternLayout extends org.apache.log4j.PatternLayout {
     return new PatternParser(pattern) {
       protected void finalizeConverter(char c) {
         if (c == 'h') {
-          addConverter(HEAP_SIZE_PATTERN_CONVERTER);
+          addConverter(new HeapSizePatternConverter());
         }
         else if (c == 'R') {
-          addConverter(REQUEST_ID_PATTERN_CONVERTER);
+          addConverter(new RequestIdPatternConverter());
         }
         else {
           super.finalizeConverter(c);
