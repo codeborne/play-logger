e047c303bc9c9f13048365cf53bd8c366fca031e
Andrei Solntsev <andrei.solntsev@gmail.com>
2013-09-02 23:12:01 +0300

    Added build script

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+MODULE="logger"
+VERSION=`grep self conf/dependencies.yml | sed "s/.*$MODULE //"`
+
+rm -fr dist
+play dependencies --sync
+play build-module
+#scp dist/*.zip codeborne.com:/var/www/repo/play-$MODULE/$MODULE-$VERSION.zip
+cp dist/*.zip /var/www/repo/play-$MODULE/$MODULE-$VERSION.zip
