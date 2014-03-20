8f726609dc72d3393fcbf10b2a798bdae71106e0
Andrei Solntsev <andrei.solntsev@gmail.com>
2014-03-20 21:23:42 +0200

    Fixed RequestLogPluginTest so that it doesn't depend on order of parameters

--- test/play/modules/logger/RequestLogPluginTest.java
+++ test/play/modules/logger/RequestLogPluginTest.java
@@ -11,7 +11,8 @@ import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Properties;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
+import static org.junit.matchers.JUnitMatchers.containsString;
 
 public class RequestLogPluginTest {
   @SuppressWarnings("deprecation")
@@ -27,13 +28,36 @@ public class RequestLogPluginTest {
   @Test
   public void passwordIsMasked() {
     setQueryString("username=anton&password=123&password2=456&newPassword=678&password=789&oldPassword=1693&age=12");
-    assertEquals("username=anton\tpassword2=*\tnewPassword=*\tage=12\toldPassword=*\tpassword=*", RequestLogPlugin.extractParams(request));
+    String maskedParams = RequestLogPlugin.extractParams(request);
+
+    assertThat(maskedParams, containsString("username=anton"));
+    assertThat(maskedParams, containsString("age=12"));
+    assertThat(maskedParams, containsString("password2=*"));
+    assertThat(maskedParams, containsString("newPassword=*"));
+    assertThat(maskedParams, containsString("oldPassword=*"));
+    assertThat(maskedParams, containsString("password=*"));
+    assertFalse(maskedParams.contains("123"));
+    assertFalse(maskedParams.contains("456"));
+    assertFalse(maskedParams.contains("67"));
+    assertFalse(maskedParams.contains("789"));
+    assertFalse(maskedParams.contains("1693"));
   }
 
   @Test
   public void cvvIsMasked() {
-    setQueryString("card.holderName=Some+Body&card.number=6789690444552800&card.validityMonth=07&card.validityYear=2015&card.cvv=907&cvv=600");
-    assertEquals("card.validityYear=2015\tcard.cvv=*\tcvv=*\tcard.number=*\tcard.validityMonth=07\tcard.holderName=Some Body", RequestLogPlugin.extractParams(request));
+    setQueryString("card.holderName=Some+Body&card.number=6789690444552800&" +
+        "card.validityMonth=07&card.validityYear=2015&card.cvv=907&cvv=600");
+    String maskedParams = RequestLogPlugin.extractParams(request);
+
+    assertThat(maskedParams, containsString("card.validityYear=2015"));
+    assertThat(maskedParams, containsString("card.validityMonth=07"));
+    assertThat(maskedParams, containsString("card.holderName=Some Body"));
+    assertThat(maskedParams, containsString("card.cvv=*"));
+    assertThat(maskedParams, containsString("cvv=*"));
+    assertThat(maskedParams, containsString("card.number=*"));
+    assertFalse(maskedParams.contains("6789690444552800"));
+    assertFalse(maskedParams.contains("907"));
+    assertFalse(maskedParams.contains("600"));
   }
 
   @Test
@@ -46,8 +70,13 @@ public class RequestLogPluginTest {
   public void postParametersAreIncluded() {
     setQueryString("id=123");
     request.contentType = "application/x-www-form-urlencoded";
-    request.body = new ByteArrayInputStream("password=123&x=y".getBytes());
-    assertEquals("id=123\tpassword=*\tx=y", RequestLogPlugin.extractParams(request));
+    request.body = new ByteArrayInputStream("password=456&x=y".getBytes());
+    String maskedParams = RequestLogPlugin.extractParams(request);
+
+    assertThat(maskedParams, containsString("id=123"));
+    assertThat(maskedParams, containsString("password=*"));
+    assertThat(maskedParams, containsString("x=y"));
+    assertFalse(maskedParams.contains("456"));
   }
 
   @Test
