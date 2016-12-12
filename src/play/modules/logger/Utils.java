e27e469c8ce45b204615e19209af1a6d684c7628
Dmitri Ess, Tõnis Aruste <dmitri.ess@codeborne.com, tonis@codeborne.com>
2016-12-12 16:43:16 +0200

    134055033 mask card numbers that have non-digit prefixes or suffixes

--- src/play/modules/logger/Utils.java
+++ src/play/modules/logger/Utils.java
@@ -3,7 +3,7 @@ package play.modules.logger;
 import java.util.regex.Pattern;
 
 class Utils {
-  private static final Pattern CARD_NUMBER_PATTERN = Pattern.compile("\\b([0-9]{6})([0-9]{6,9})([0-9]{4})\\b");
+  private static final Pattern CARD_NUMBER_PATTERN = Pattern.compile("((?:\\b|\\D)[0-9]{6})([0-9]{6,9})([0-9]{4}(?:\\b|\\D))");
 
   public static String maskCardNumber(String text) {
     return CARD_NUMBER_PATTERN.matcher(text).replaceAll("$1******$3");
