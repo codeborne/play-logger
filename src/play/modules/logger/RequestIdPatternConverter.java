9449e347183ebbcfbb646b2aaf3a4a8635c71079
Kunnar Klauks, Tõnis Aruste <kunnar@codeborne.com, tonis@codeborne.com>
2017-03-03 15:05:24 +0200

    request id can be passed through MDC

--- src/play/modules/logger/RequestIdPatternConverter.java
+++ src/play/modules/logger/RequestIdPatternConverter.java
@@ -2,16 +2,15 @@ package play.modules.logger;
 
 import org.apache.log4j.helpers.PatternConverter;
 import org.apache.log4j.spi.LoggingEvent;
+import org.slf4j.MDC;
 import play.mvc.Http;
 
 class RequestIdPatternConverter extends PatternConverter {
 
   @Override protected String convert(LoggingEvent event) {
-    String threadName = currentThreadName();
     Http.Request request = Http.Request.current();
-    if (request == null) return threadName;
-    Object rid = request.args.get("requestId");
-    return rid == null ? threadName : rid.toString();
+    Object rid = request != null ? request.args.get("requestId") : MDC.get("requestId");
+    return rid == null ? currentThreadName() : rid.toString();
   }
 
   protected String currentThreadName() {
--- test/play/modules/logger/RequestIdPatternConverterTest.java
+++ test/play/modules/logger/RequestIdPatternConverterTest.java
@@ -1,12 +1,13 @@
 package play.modules.logger;
 
 import org.apache.log4j.Category;
+import org.apache.log4j.MDC;
 import org.apache.log4j.spi.LoggingEvent;
 import org.junit.After;
 import org.junit.Test;
 import play.mvc.Http;
 
-import static org.apache.log4j.Priority.WARN;
+import static org.apache.log4j.Level.WARN;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.mock;
@@ -15,11 +16,22 @@ public class RequestIdPatternConverterTest {
 
   @After
   public void tearDown() throws Exception {
+    MDC.clear();
     Http.Request.current.set(null);
   }
 
   @Test
-  public void returnsThreadNameIfRequestIsMissing() throws Exception {
+  public void returnRequestIdFromMDCIfRequestIsMissing() throws Exception {
+    Http.Request.current.remove();
+    MDC.put("requestId", "job-exec-273");
+
+    LoggingEvent event = new LoggingEvent("INFO", mock(Category.class), WARN, "message", null);
+
+    assertThat(new RequestIdPatternConverter().convert(event), is("job-exec-273"));
+  }
+
+  @Test
+  public void returnsThreadNameIfRequestAndMDCIsMissing() throws Exception {
     Thread.currentThread().setName("Thread name");
     LoggingEvent event = new LoggingEvent("INFO", mock(Category.class), WARN, "message", null);
 
@@ -39,7 +51,7 @@ public class RequestIdPatternConverterTest {
 
   @Test
   @SuppressWarnings("deprecation")
-  public void returnsThreadNameIfRequestIdIsMissing() throws Exception {
+  public void returnsThreadNameIfRequestIdInRequestIsMissing() throws Exception {
     Thread.currentThread().setName("Thread name");
     Http.Request.current.set(new Http.Request());
     LoggingEvent event = new LoggingEvent("INFO", mock(Category.class), WARN, "message", null);
